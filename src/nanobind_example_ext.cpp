0031146341d05061c031d4b6a6fde49f9422b9a4
Stefan Jeske <stefan.jeske@rwth-aachen.de>
2024-03-18 22:55:44 +0100

    minimal example for bad cast eigen error on macos

--- src/nanobind_example_ext.cpp
+++ src/nanobind_example_ext.cpp
@@ -1,9 +1,28 @@
+#include <Eigen/Dense>
 #include <nanobind/nanobind.h>
+#include <nanobind/eigen/dense.h>
+#include <iostream>
 
 namespace nb = nanobind;
 
 using namespace nb::literals;
 
+Eigen::Vector3d zero_pivot = Eigen::Matrix<double, 3, 1>(0.0, 0.0, 0.0);
+
 NB_MODULE(nanobind_example_ext, m) {
-    m.def("add", [](int a, int b) { return a + b; }, "a"_a, "b"_a);
+    // Does not work
+    m.def("add_rotation", [](double angle, Eigen::Vector3d axis, Eigen::Vector3d pivot, bool also_at_rest_post){
+        std::cout << "pivot: " << pivot << std::endl;
+    }, "angle_deg"_a, "axis"_a, "pivot"_a = Eigen::Vector3d(0.0, 0.0, 0.0), "also_at_rest_pose"_a = true);
+
+	// Does not work
+    // m.def("add_rotation", [](double angle, Eigen::Vector3d axis, Eigen::Vector3d pivot, bool also_at_rest_post){
+    //     std::cout << "pivot: " << pivot << std::endl;
+    // }, "angle_deg"_a, "axis"_a, nb::arg("pivot") = zero_pivot, "also_at_rest_pose"_a = true);
+    
+    // Works
+    // m.def("add_rotation", [](double angle, Eigen::Vector3d axis, Eigen::Vector3d pivot, bool also_at_rest_post){
+    //     std::cout << "pivot: " << pivot << std::endl;
+    // }, "angle_deg"_a, "axis"_a, "pivot"_a, "also_at_rest_pose"_a = true);
+
 }
